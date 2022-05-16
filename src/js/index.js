47da9cd6fd3aea0126f112d6971bf89650140d95
Adelmo Santos <adelmojsantos1985@gmail.com>
2022-05-16 19:09:46 -0300

    Adicionando javascript - final

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,21 @@ console.log(characters)
 
 characters.forEach((character) => {
   character.addEventListener('mouseenter', () => {
+    const selectedId = character.attributes.id.value
+
+    if (selectedId === 'ultron') return
     const selectedCharacter = document.querySelector('.selected')
     selectedCharacter.classList.remove('selected')
     character.classList.add('selected')
 
-    const selectedId = character.attributes.id.value
+
+    const player1Img = document.getElementById('player-1-character')
+    player1Img.src = `./src/images/${selectedId}.png`
+
+    const selectedName = character.getAttribute('data-name')
+
+    const player1Name = document.getElementById("player-1-name")
+    player1Name.innerHTML = selectedName
   })
 })
 
